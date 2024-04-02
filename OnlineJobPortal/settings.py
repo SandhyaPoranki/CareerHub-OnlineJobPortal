2bfe68aa449be34ec073c235aad6c5312ac9bd2b
PORANKI SANDHYA <95462067+SandhyaPoranki@users.noreply.github.com>
2024-04-03 00:14:35 +0530

    Update settings.py

--- OnlineJobPortal/settings.py
+++ OnlineJobPortal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-203z4$&*ze3!3zxn_ki#p!nk3e_kahxc+id(ra!p26kr%-!k5a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
